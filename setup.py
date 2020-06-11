0f475438a6465c3d308bf1781b3b6d936ccc0405
david <davidventura27@gmail.com>
2020-06-11 12:44:27 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,15 @@ setup(
     name="pytest_snmpserver",
     version="0.1",
     packages=find_packages(),
-    long_description="blah",
+    short_description="SNMP server as a pytest plugin",
+    long_description="SNMP server as a pytest plugin",
     long_description_content_type="text/markdown",
     python_requires=">=3.6",
     install_requires=[],
-    entry_points={"pytest11": ["pytest_snmpserver = pytest_snmpserver.pytest_plugin"]}
-)
+    entry_points={"pytest11": ["pytest_snmpserver = pytest_snmpserver.pytest_plugin"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+    )
